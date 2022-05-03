a73b47b134ef57148a649982a184d1815c4960c5
nitishpant <nitishpant9@gmail.com>
2022-05-03 18:48:11 +0530

    added newProduct route

--- backend/config/database.js
+++ backend/config/database.js
@@ -4,7 +4,7 @@ const mongoose = require('mongoose');
 const connectDatabase=()=>{
     mongoose.connect(process.env.DB_URI).
     then((x) =>{
-       console.log(`mongodb connected with server ${x.connection.host}`);
+       console.log(`mongodb connected with server ${x.connection.port}`);
     }).catch((x)=>{
         console.log(`mongodb error : ${x}`);
     });
--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,7 @@ const connectDatabase = require("./config/database");
 
 dotenv.config({path:"backend/config/config.env"});
 
+connectDatabase();
 
 app.listen(process.env.PORT, ()=>{
     console.log(`server started at : ${process.env.PORT}`)
