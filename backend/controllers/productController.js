4c7bd9e13aa153942760d69f9ccbc30bb640719f
Nitish Pant <40199652+nitishpant@users.noreply.github.com>
2022-05-03 19:05:01 +0530

    db functions
    * added database mongodb
    
    * added newProduct route

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -1,5 +1,28 @@
-exports.getAllProducts = (req, res)=>{
+const Product = require("../models/productModel")
 
-    res.status(200).json({message:"product working fine"});
+
+//create product -only admins
+exports.createProduct = async (req, res, next)=>{
+    try{
+        const product = await Product.create(req.body);
+        res.status(201).json({
+            success:true,
+            product
+        })
+    }
+    catch(err){
+        res.status(400).json({
+            error:err
+        })
+    }
+}
+
+//get all products
+exports.getAllProducts = async (req, res)=>{ 
+    const products = await Product.find();
+    res.status(200).json({
+        success:true,
+        products
+    });
 
 }
