1da7b3f5b404414d20d560998c4082fa83913b63
Nitish Pant <40199652+nitishpant@users.noreply.github.com>
2022-05-09 00:10:42 +0530

    forgot password (#6)
    * Update README.md
    
    * indentaion
    
    * added api features search, filter
    
    * added pagination
    
    * authentication start
    
    * authentication
    
    * authentication complete
    
    * forgot password mail added

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -3,6 +3,9 @@ const catchAsyncErrors = require("../middleware/catchAsyncErrors");
 const User = require("../models/userModel");
 const sendToken = require("../utils/jwtToken");
 
+const sendEmail = require("../utils/sendEmail");
+
+
 //register a user
 exports.registerUser = catchAsyncErrors(async (req, res) => {
   const { name, email, password, role } = req.body;
@@ -40,6 +43,58 @@ exports.loginUser = catchAsyncErrors(async (req, res, next) => {
 });
 
 //user logout
+
+exports.logoutUser = catchAsyncErrors(async (req, res, next) => {
+  res.cookie("token", null, {
+    expires: new Date(Date.now()),
+    httpOnly: true,
+  });
+
+  res.status(200).json({
+    success: true,
+    message: "Logged out",
+  });
+});
+
+//fogot password
+exports.forgotPassword = catchAsyncErrors(async (req, res, next) => {
+  const user = await User.findOne({ email: req.body.email });
+  if (!user) {
+    return next(new ErrorHandler("User not found", 404));
+  }
+
+  //get resetPassword Token
+  const resetToken = user.getResetPasswordToken();
+  await user.save({
+    validateBeforeSave: false,
+  });
+  const resetPasswordURL = `${req.protocol}://${req.get(
+    "host"
+  )}/api/v1/password/reset/${resetToken}`; //http://localhost/api/v1/reset/${resetToken}
+
+  const message = `Your password reset URL is : \n \n ${resetPasswordURL}`;
+
+  try {
+    await sendEmail({
+      email: user.email,
+      subject: `Ecommerce Password Recovery`,
+      message,
+    });
+    res.status(200).json({
+      success: true,
+      message: `Email to : ${user.email} sent successfully`,
+    });
+  } catch (error) {
+    user.resetPasswordToken = undefined;
+    user.resetPasswordExpire = undefined;
+
+    await user.save({
+      validateBeforeSave: false,
+    });
+    return next(new ErrorHandler(error.message, 500));
+  }
+});
+
 exports.logoutUser = catchAsyncErrors( async(req, res, next)=>{
   res.cookie("token",null,{   
     expires: new Date(Date.now()),
@@ -52,3 +107,4 @@ exports.logoutUser = catchAsyncErrors( async(req, res, next)=>{
     message:"Logged out"
   })
 })
+
