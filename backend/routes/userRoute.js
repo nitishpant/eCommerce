9b1a583669644263cd6952a6fcee40d82365d8be
Nitish Pant <40199652+nitishpant@users.noreply.github.com>
2022-05-11 00:39:49 +0530

    Admin routes (#7)
    * Update README.md
    
    * indentaion
    
    * added api features search, filter
    
    * added pagination
    
    * authentication start
    
    * authentication
    
    * authentication complete
    
    * forgot password mail added
    
    * gitignore
    
    * added admin routes

--- backend/routes/userRoute.js
+++ backend/routes/userRoute.js
@@ -7,15 +7,36 @@ const {
 
   forgotPassword,
 
+  resetPassword,
+  getUserDetails,
+  updatePassword,
+  getAllUsers,
+  getSingleUserDetails,
 
 } = require("../controllers/userController");
+const { isAuthenticatedUser, authorizeRoles } = require("../middleware/auth");
 
 const router = express.Router();
 
 router.route("/register").post(registerUser);
 router.route("/login").post(loginUser);
 
+
+router.route("/password/update").put(isAuthenticatedUser, updatePassword);
 router.route("/password/forgot").post(forgotPassword);
+router.route("/password/reset/:token").put(resetPassword);
+
+router.route("/me").get(isAuthenticatedUser, getUserDetails);
+
+router
+  .route("/admin/users")
+  .get(isAuthenticatedUser, authorizeRoles("admin"), getAllUsers);
+router
+  .route("/admin/user/:id")
+  .get(isAuthenticatedUser, authorizeRoles("admin"), getSingleUserDetails);
+
+router.route("/password/forgot").post(forgotPassword);
+
 
 
 router.route("/logout").get(logoutUser);
